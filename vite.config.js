a552873f76ab257e88274bbb76e33e53a06bcece
Sooraj <soorajnambiar8@gmail.com>
2024-05-22 22:12:53 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Kaizen-fronend/",
+  base:"/Kaizen-Fronend/",
   plugins: [react()],
 })
